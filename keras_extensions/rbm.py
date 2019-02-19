a8ce58ce2ef2259e31cbb24230315ac4d1621f6e
Peter Kasson <kasson@gmail.com>
2019-02-19 05:47:57 -0800

    bugfix for updates

--- keras_extensions/rbm.py
+++ keras_extensions/rbm.py
@@ -48,7 +48,6 @@ class RBM(Layer):
 			self.is_persistent = persistent
 			self.nb_gibbs_steps = nb_gibbs_steps
 
-		self.my_updates = []
 		self.init = initializers.get(init)
 		self.activation = activations.get(activation)
 		self.hidden_dim = hidden_dim
@@ -237,7 +236,7 @@ class RBM(Layer):
 		y, x_rec = loss(chain_start, x)
 
 		if(self.is_persistent):
-			self.my_updates = [(self.persistent_chain, x_rec)]
+			self.add_updates([(self.persistent_chain, x_rec)])
 
 		return y
 
